c164f4a4223bff6b9fd2491a5f70490057ef8866
Carlos Medina <carlos.w.med@gmail.com>
2017-12-12 01:22:17 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var sass = require('gulp-sass')
+var plumber = require('gulp-plumber')
+var autoprefixer = require('gulp-autoprefixer')
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: './dist'
+  })
+
+  gulp.watch('./scss/**/*.scss', ['sass'])
+  gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('plumber', ['sass'], function() {
+  gulp
+    .src('./src/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    // .pipe(uglify())
+    .pipe(plumber.stop())
+    .pipe(gulp.dest('./'))
+})
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp
+    .src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(
+      autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+        cascade: true
+      })
+    )
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('default', ['serve'])
