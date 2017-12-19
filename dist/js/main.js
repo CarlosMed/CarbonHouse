ca76523415429566becae124a927987ada42f132
Carlos Medina <carlos.w.med@gmail.com>
2017-12-18 22:20:19 -0500

    Add some minor animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,1 @@
-function $(selection) {
-  return document.querySelector(selection)
-}
-
-$('.navbar__hamburger').addEventListener('click', e => {
-    $('.navbar__links').className += ' open'
-})
-
-window.addEventListener('scroll', e => {
-  if ($('.navbar__links').className === 'navbar__links open') {
-    $('.navbar__links').classList.remove('open')
-  }
-})
+console.log('hello');
