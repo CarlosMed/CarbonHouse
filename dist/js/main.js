a4bb047f4d1524a0a39591d30db21aa9e07bb084
Carlos Medina <carlos.w.med@gmail.com>
2017-12-20 00:13:50 -0500

    fix the firefox bug that had to do with the clipping on the newsletter and event hover, started on some JS event handler, and imported greensocks into html.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,7 @@
-console.log('hello');
+function $(selection) {
+  return document.querySelector(selection)
+}
+
+let links = $('.navbar__links')
+
+$('.navbar__hamburger').addEventListener('click', () => links.className = 'navbar__links open');
